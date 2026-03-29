2b2824dccfe537abe6876ee02c8eb28b8dc4089c
guentxim <ximuelguenther@hotmail.com>
2026-03-29 17:13:10 +0200

    test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "SPIComCntrl.h"
 #include "config.h"
 #include "mbed.h"
+#include "InverseKinematics3Leg.h"
 
 // TODOs:
 // - Start with understanding this project
@@ -9,6 +10,26 @@ int main()
 {
     SPIComCntrl spiComCntrl;
     spiComCntrl.enable();
+
+    InverseKinematics3Leg ik;
+
+    // Example usage of InverseKinematics3Leg
+    InverseKinematics3Leg::Input ikInput;
+    ikInput.roll = 0.1;   // [rad] roll
+    ikInput.pitch = 0.1;  // [rad] pitch
+    ikInput.h = 100.0;    // [mm] height
+
+    InverseKinematics3Leg::Result ikResult = ik.compute(ikInput);
+    
+    if (ikResult.success) {
+            printf("IK computation successful!\n");
+            for (size_t i = 0; i < 3; ++i) {
+                printf("Leg %zu: alpha = %.2f deg, rod error = %.2f mm\n",
+                        i + 1, ikResult.alphaDeg[i], ikResult.rodError[i]);
+            }
+        } else {
+            printf("IK computation failed: %s\n", ikResult.errorMessage.c_str());
+        }    
 
     while (true) {
         thread_sleep_for(1000);
