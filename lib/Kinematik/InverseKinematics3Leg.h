ee5ba311e1d7504821c66013d774689b07266e97
Ximuel <ximuelguentherhhotmail.com>
2026-04-09 13:43:15 +0200

    New geometry parameter

--- lib/Kinematik/InverseKinematics3Leg.h
+++ lib/Kinematik/InverseKinematics3Leg.h
@@ -9,10 +9,10 @@ class InverseKinematics3Leg
 public:
     struct Geometry
     {
-        float r0 = 61.001;          // radius of motor axes
-        float r1 = 72.5;            // servo horn length
-        float r2 = 101.5;            // rod length
-        float rp = 215.0 / 2.0;     // platform radius
+        float r0 = 61.001;              // radius of motor axes
+        float r1 = 85.0;                // servo horn length
+        float r2 = 117.0;               // rod length
+        float rp = 215.0 / 2.0;         // platform radius
 
         // Motor azimuth angles [rad]
         std::array<float, 3> TH = {
@@ -26,7 +26,7 @@ public:
     {
         float roll  = 0.0;   // [rad]
         float pitch = 0.0;   // [rad]
-        float h     = 100.0; // [same length unit as geometry]
+        float h     = 110.5; // [same length unit as geometry]
     };
 
     struct Result
