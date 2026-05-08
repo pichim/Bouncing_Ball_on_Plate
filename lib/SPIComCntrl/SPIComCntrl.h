6df2dada997011691fc66faf8d3b4416ac83e7c5
guentxim <ximuelguenther@hotmail.com>
2026-05-08 16:54:24 +0200

    Regler updates

--- lib/SPIComCntrl/SPIComCntrl.h
+++ lib/SPIComCntrl/SPIComCntrl.h
@@ -50,7 +50,7 @@ private:
     static constexpr float BALL_CTRL_KP = 0.05f;
     static constexpr float BALL_CTRL_KI = 0.004f;
     static constexpr float BALL_CTRL_TAU_V = 0.9f;
-    static constexpr float BALL_CTRL_TAU_f = 0.1f;
+    static constexpr float BALL_CTRL_TAU_f = 0.12f;
     static constexpr float BALL_CTRL_TAU_R_O = 0.01f;
     static constexpr float BALL_CTRL_KD = BALL_CTRL_KP * (BALL_CTRL_TAU_V - BALL_CTRL_TAU_f);
     static constexpr float VISION_TIMEOUT = 1.0F; // seconds
